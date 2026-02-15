bf15e33552ba39c920d25ccd5515fd7f69448565
Charlie Federspiel <cfederspiel@Charlies-MacBook-Pro.local>
2026-02-14 16:27:49 -0800

    invoke cfn template to launch DVC instance.

---
+++ terraform/dcv.tf
@@ -0,0 +1,37 @@
+resource "aws_cloudformation_stack" "nice_dcv" {
+  name = "nice-dcv-ubuntu"
+
+  template_body = file("${path.module}/Ubuntu-NICE-dcv.yaml")
+
+  parameters = {
+    ec2KeyPair    = var.ec2_key_pair
+    instanceType  = var.ec2_instance_type
+    vpcID         = aws_vpc.main.id
+    subnetID      = aws_subnet.sub-pub1.id
+    ingressIPv4   = var.user-cidr-ipv4
+    listenPort    = var.dcv_listen_port
+    sessionType   = var.dcv_session_type
+    osVersion     = var.dcv_os_version
+    volumeSize    = var.dcv_volume_size
+    assignStaticIP       = "No"
+    displayPublicIP      = "Yes"
+    allowSSHport         = "Yes"
+    installDocker        = "Yes"
+    installWebmin        = "No"
+    enableCloudFront     = "No"
+    enableAGA            = "No"
+    enableBackup         = "No"
+    allowWebServerPorts  = "No"
+    ec2TerminationProtection = "No"
+  }
+
+  capabilities = ["CAPABILITY_IAM"]
+
+  tags = {
+    Name = "nice-dcv-ubuntu"
+  }
+}
+
+output "dcv_stack_outputs" {
+  value = aws_cloudformation_stack.nice_dcv.outputs
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,23 @@ variable "user-cidr-ipv4" {
   description = "CIDR block of users"
   type = string
 }
+variable "dcv_listen_port" {
+  description = "NICE DCV server listen port"
+  type        = number
+  default     = 8443
+}
+variable "dcv_session_type" {
+  description = "DCV session type: virtual, console, gpu-console, or gpu-virtual"
+  type        = string
+  default     = "virtual"
+}
+variable "dcv_os_version" {
+  description = "Ubuntu OS version for DCV instance"
+  type        = string
+  default     = "Ubuntu 24.04 (arm64)"
+}
+variable "dcv_volume_size" {
+  description = "EBS volume size in GiB"
+  type        = number
+  default     = 25
+}
