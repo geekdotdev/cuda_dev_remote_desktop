5fd970f8858ec0f32c222dc26036989415a2bcc3
Charlie Federspiel <cfederspiel@Charlies-MacBook-Pro.local>
2026-02-14 16:43:07 -0800

    fixed ec2 instance resource name in outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,9 +4,9 @@ output "Region" {
 }
 output "InstancePublicIp" {
   description = "instance ip address"
-  value	= resource.aws_instance.public_linux.public_ip
+  value	= resource.ec2Instance.public_linux.public_ip
 }
 output "InstancePublicDns" {
   description = "instance ip address"
-  value	= resource.aws_instance.public_linux.public_dns
+  value	= resource.ec2Instance.public_linux.public_dns
 }
